e7f7e0da0e60a8ca4555669343204ec2dc145e74
ZIGGURAT25 <72934907+ZIGGURAT25@users.noreply.github.com>
2025-12-28 08:19:14 +0530

    Add batches to timetable and update last updated date
    Updated timetable data to include batches for each period and modified the last updated date.

--- data.js
+++ data.js
@@ -45,36 +45,37 @@ window.timetableData = {
     days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     schedule: {
         Monday: [
-            { period: 1, time: "8:00 AM - 8:50 AM", courseCode: "CE23631", room: "A204 (A Block)" },
-            { period: 2, time: "9:00 AM - 9:50 AM", courseCode: "CE23613", room: "A310 (A Block)" },
-            { period: 3, time: "10:00 AM - 10:50 AM", courseCode: "CE23611", room: "A311 (A Block)" },
-            { period: 4, time: "11:00 AM - 11:50 AM", courseCode: "CE23613", room: "A303 (A Block)" },
-            { period: 5, time: "12:00 PM - 12:50 PM", courseCode: "CE23631", room: "A108 (A Block)" },
-            { period: 6, time: "2:10 PM - 3:00 PM", courseCode: "CE23612", room: "A303 (A Block)" }
+            { period: 1, time: "8:00 AM - 8:50 AM", courseCode: "CE23631", room: "A204 (A Block)", batches: [1, 2] },
+            { period: 2, time: "9:00 AM - 9:50 AM", courseCode: "CE23613", room: "A310 (A Block)", batches: [1, 2] },
+            { period: 3, time: "10:00 AM - 10:50 AM", courseCode: "CE23611", room: "A311 (A Block)", batches: [1, 2] },
+            { period: 4, time: "11:00 AM - 11:50 AM", courseCode: "CE23613", room: "A303 (A Block)", batches: [1, 2] },
+            { period: 5, time: "12:00 PM - 12:50 PM", courseCode: "CE23631", room: "A108 (A Block)", batches: [1, 2] },
+            { period: 6, time: "2:10 PM - 3:00 PM", courseCode: "CE23612", room: "A303 (A Block)", batches: [1, 2] }
         ],
         Tuesday: [
-            { period: 1, time: "11:00 AM - 11:50 AM", courseCode: "CE23612", room: "A108 (A Block)" },
-            { period: 2, time: "12:00 PM - 12:50 PM", courseCode: "CE23611", room: "A309 (A Block)" },
-            { period: 3, time: "3:00 PM - 3:50 PM", courseCode: "CE23632", room: "A309 (A Block)" },
-            { period: 4, time: "4:00 PM - 4:50 PM", courseCode: "CE23632", room: "A309 (A Block)" }
+            { period: 1, time: "11:00 AM - 11:50 AM", courseCode: "CE23612", room: "A108 (A Block)", batches: [1, 2] },
+            { period: 2, time: "12:00 PM - 12:50 PM", courseCode: "CE23611", room: "A309 (A Block)", batches: [1, 2] },
+            { period: 3, time: "3:00 PM - 3:50 PM", courseCode: "CE23632", room: "A309 (A Block)", batches: [1, 2] },
+            { period: 4, time: "4:00 PM - 4:50 PM", courseCode: "CE23632", room: "A309 (A Block)", batches: [1, 2] }
         ],
         Wednesday: [
-            { period: 1, time: "12:00 PM - 12:50 PM", courseCode: "CE23611", room: "A308 (A Block)" }
+            { period: 1, time: "08:00 AM - 09:40 AM", courseCode: "CE23631", room: "JR2", subType: "lab", batches: [1] },
+            { period: 5, time: "12:00 PM - 12:50 PM", courseCode: "CE23611", room: "A308 (A Block)", batches: [1, 2] }
         ],
         Thursday: [
-            { period: 1, time: "1:20 PM - 2:10 PM", courseCode: "CE23611", room: "A108 (A Block)" },
-            { period: 2, time: "3:00 PM - 3:50 PM", courseCode: "CE23632", room: "A203 (A Block)" },
-            { period: 3, time: "4:00 PM - 4:50 PM", courseCode: "CE23632", room: "A203 (A Block)" }
+            { period: 1, time: "1:20 PM - 2:10 PM", courseCode: "CE23611", room: "A108 (A Block)", batches: [1, 2] },
+            { period: 2, time: "3:00 PM - 3:50 PM", courseCode: "CE23632", room: "A203 (A Block)", batches: [1, 2] },
+            { period: 3, time: "4:00 PM - 4:50 PM", courseCode: "CE23632", room: "A203 (A Block)", batches: [1, 2] }
         ],
         Friday: [
-            { period: 1, time: "8:00 AM - 8:50 AM", courseCode: "CE23631", room: "A303 (A Block)" },
-            { period: 2, time: "9:00 AM - 9:50 AM", courseCode: "CE23613", room: "A202 (A Block)" },
-            { period: 3, time: "11:00 AM - 11:50 AM", courseCode: "CE23612", room: "A203 (A Block)" },
-            { period: 4, time: "11:50 AM - 1:20 PM", courseCode: "CE23631", room: "TLGL4 (Techlounge)", subType: "lab" }
+            { period: 1, time: "8:00 AM - 8:50 AM", courseCode: "CE23631", room: "A303 (A Block)", batches: [1, 2] },
+            { period: 2, time: "9:00 AM - 9:50 AM", courseCode: "CE23613", room: "A202 (A Block)", batches: [1, 2] },
+            { period: 3, time: "11:00 AM - 11:50 AM", courseCode: "CE23612", room: "A203 (A Block)", batches: [1, 2] },
+            { period: 4, time: "11:50 AM - 1:20 PM", courseCode: "CE23631", room: "TLGL4 (Techlounge)", subType: "lab", batches: [2] }
         ],
         Saturday: [],
         Sunday: []
     }
 };
 
-window.lastUpdatedDate = '2025-12-27T20:30:00';
+window.lastUpdatedDate = '2025-12-28T08:15:00';

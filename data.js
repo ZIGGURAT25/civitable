2dcaa63e491c24204091a56ff86f751c54236918
vampdued <vampdued@proton.me>
2026-07-05 09:26:56 +0530

    Modify course and timetable data
    Updated course and timetable data, added new courses and schedules.

--- data.js
+++ data.js
@@ -1,81 +1,76 @@
 window.courseData = {
-    "CE23611": {
-        title: "Design of Steel Structures",
-        shortCode: "DSS",
-        staff: "S Geetha",
-        staffCode: "CE_SG",
+    "CE23712": {
+        title: "Hydrology",
+        shortCode: "HYD",
+        staff: "Goutham Priya M",
+        staffCode: "CE_GP",
+        credits: "3",
+        type: "theory"
+    },
+    "CE23711": {
+        title: "Estimation, Costing and Valuation Engineering",
+        shortCode: "ECV",
+        staff: "Madhava Perumal R",
+        staffCode: "CE_MP",
         credits: "4",
         type: "theory"
     },
-    "CE23612": {
-        title: "Construction, Planning, Scheduling and Management",
-        shortCode: "CPM",
-        staff: "M Umamaguesvari",
-        staffCode: "CE_MU",
+    "CE23F11": {
+        title: "Intelligent Transport System",
+        shortCode: "ITS",
+        staff: "Yugasini S",
+        staffCode: "CE_YS",
         credits: "3",
         type: "theory"
     },
-    "CE23613": {
-        title: "Structural Dynamics and Earthquake Engineering",
-        shortCode: "SDE",
-        staff: "Balkis Banu W",
-        staffCode: "CE_BB",
-        credits: "3",
-        type: "theory"
-    },
-    "CE23631": {
-        title: "Structural Design and Drawing",
-        shortCode: "SDD",
-        staff: "Mahamood Ul Hasan N",
-        staffCode: "CE_MH",
-        credits: "4",
-        type: "lot"
-    },
-    "CE23632": {
-        title: "Design Thinking and Innovation",
-        shortCode: "DTI",
-        staff: "Jeya Arthi A J",
-        staffCode: "CE_JA",
-        credits: "3",
-        type: "theory"
+    "CE23721": {
+        title: "Building Information Modelling",
+        shortCode: "BIM",
+        staff: "Madhava Perumal R",
+        staffCode: "CE_MP",
+        credits: "2",
+        type: "lab"
     }
 };
 
 window.timetableData = {
   days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
   schedule: {
-    Monday: [
-      { period: 1, time: "8:00 AM - 8:45 AM", courseCode: "CE23631", room: "B213" },
-      { period: 2, time: "9:00 AM - 9:45 AM", courseCode: "CE23613", room: "B213" },
-      { period: 3, time: "10:00 AM - 10:45 AM", courseCode: "CE23611", room: "B213" },
-      { period: 4, time: "11:00 AM - 11:45 AM", courseCode: "CE23613", room: "B213" },
-      { period: 5, time: "12:00 PM - 12:45 PM", courseCode: "CE23631", room: "B213" },
-      { period: 6, time: "2:10 PM - 2:50 PM", courseCode: "CE23612", room: "B213" }
-    ],
+    Monday: [],
     Tuesday: [
-      { period: 1, time: "11:00 AM - 11:45 AM", courseCode: "CE23612", room: "B213" },
-      { period: 2, time: "12:00 PM - 12:45 PM", courseCode: "CE23611", room: "B213" },
-      { period: 3, time: "2:55 PM - 3:40 PM", courseCode: "CE23632", room: "B213" },
-      { period: 4, time: "3:55 PM - 4:40 PM", courseCode: "CE23632", room: "B213" }
+      { period: 1, time: "10:00 AM - 11:40 AM", courseCode: "CE23721", room: "TIFAC I01", subType: "lab" },
+      { period: 2, time: "12:00 PM - 12:50 PM", courseCode: "CE23F11", room: "A311" },
+      { period: 3, time: "2:10 PM - 3:00 PM", courseCode: "CE23712", room: "A311" },
+      { period: 4, time: "3:00 PM - 3:50 PM", courseCode: "CE23711", room: "A311" },
+      { period: 5, time: "4:00 PM - 4:50 PM", courseCode: "CE23F11", room: "A311" }
     ],
     Wednesday: [
-      { period: 1, time: "11:00 AM - 11:45 AM", courseCode: "CE23611", room: "B213" },
-      { period: 2, time: "12:00 PM - 12:45 PM", courseCode: "CE23611", room: "B213" }
+      { period: 1, time: "9:00 AM - 9:50 AM", courseCode: "CE23712", room: "A311" },
+      { period: 2, time: "10:00 AM - 11:40 AM", courseCode: "CE23721", room: "TIFAC I01", subType: "lab" },
+      { period: 3, time: "1:20 PM - 2:10 PM", courseCode: "CE23711", room: "A311" },
+      { period: 4, time: "2:10 PM - 3:00 PM", courseCode: "CE23711", room: "A311" }
     ],
     Thursday: [
-      { period: 1, time: "1:20 PM - 2:05 PM", courseCode: "CE23631", room: "B213", subType: "lab" },
-      { period: 2, time: "2:10 PM - 2:50 PM", courseCode: "CE23631", room: "B213", subType: "lab" },
-      { period: 3, time: "2:55 PM - 3:40 PM", courseCode: "CE23632", room: "B213" },
-      { period: 4, time: "3:55 PM - 4:40 PM", courseCode: "CE23632", room: "B213" }
+      { period: 1, time: "8:00 AM - 8:50 AM", courseCode: "CE23712", room: "A311" },
+      { period: 2, time: "9:00 AM - 9:50 AM", courseCode: "CE23712", room: "A311" },
+      { period: 3, time: "10:00 AM - 11:40 AM", courseCode: "CE23721", room: "TIFAC I01", subType: "lab" },
+      { period: 4, time: "1:20 PM - 3:00 PM", courseCode: "CE23721", room: "TIFAC I01", subType: "lab" }
     ],
     Friday: [
-      { period: 1, time: "8:00 AM - 8:45 AM", courseCode: "CE23631", room: "B213" },
-      { period: 2, time: "9:00 AM - 9:45 AM", courseCode: "CE23613", room: "B213" },
-      { period: 3, time: "11:00 AM - 11:45 AM", courseCode: "CE23612", room: "B213" }
+      { period: 1, time: "1:20 PM - 2:10 PM", courseCode: "CE23712", room: "A311" },
+      { period: 2, time: "2:10 PM - 3:00 PM", courseCode: "CE23711", room: "A311" },
+      { period: 3, time: "3:00 PM - 3:50 PM", courseCode: "CE23F11", room: "A311" },
+      { period: 4, time: "4:00 PM - 4:50 PM", courseCode: "CE23F11", room: "A311" }
     ],
-    Saturday: [],
+    Saturday: [
+      { period: 1, time: "8:00 AM - 8:50 AM", courseCode: "CE23712", room: "A311" },
+      { period: 2, time: "9:00 AM - 9:50 AM", courseCode: "CE23711", room: "A311" },
+      { period: 3, time: "10:00 AM - 10:50 AM", courseCode: "CE23711", room: "A311" },
+      { period: 4, time: "11:00 AM - 11:50 AM", courseCode: "CE23F11", room: "A311" },
+      { period: 5, time: "12:00 PM - 12:50 PM", courseCode: "CE23F11", room: "A311" }
+    ],
     Sunday: []
   }
 };
 
-window.lastUpdatedDate = '2026-02-05T18:45:00';
+window.lastUpdatedDate = '2026-07-05T09:21:00';
